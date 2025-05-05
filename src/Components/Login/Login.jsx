ee77a9df90f77ff66df861ab84645830729bc01f
Sara-Rubaya <sararubaya4800@gmail.com>
2025-05-05 23:21:13 +0600

    added forget password & reset password

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import { auth } from '../../Firebase.init';
+import { Link } from 'react-router';
 
 const Login = () => {
+    const [success, setSuccess] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const emailRef = useRef();
 
     const handleLogin = e =>{
         e.preventDefault();
@@ -8,6 +14,45 @@ const Login = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
+
+        //reset
+        setSuccess(false);
+        setErrorMessage('');
+
+
+        //login user
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+            console.log(result);
+            if(result.user.emailVerified){
+                alert('Please varify your email.address.')
+            }
+            else{
+                setSuccess(true);
+            }
+            setSuccess(true);
+        })
+        .catch(error =>{
+            console.log(error);
+            setErrorMessage(error.message)
+        })
+    }
+
+    const handleForgetPassword = () =>{
+        console.log(emailRef.current.value);
+        const email = emailRef.current.value;
+
+        setErrorMessage('');
+
+        //send password reset email
+        sendPasswordResetEmail(auth, email)
+        .then(()=>{
+            alert('A password reset email is sent.please check your email.')
+        })
+        .catch(error =>{
+             setErrorMessage(error.message);
+        })
+
     }
     return (
 
@@ -17,12 +62,20 @@ const Login = () => {
       <h1 className="text-5xl font-bold">Login now!</h1>
         <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" name='email' className="input" placeholder="Email" />
+          <input type="email" name='email' ref={emailRef} className="input" placeholder="Email" />
           <label className="label">Password</label>
           <input type="password" name='password' className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
+          <div onClick={handleForgetPassword}><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Login</button>
         </form>
+
+        <p>New to this Website? Please <Link className='text-blue-500 underline' to="/signUp">Sign Up</Link></p>
+        {
+            errorMessage && <p className='text-red-500'>{errorMessage}</p>
+        }
+        {
+            success && <p className='text-green-500'>User logged in successfully!</p>
+        }
       </div>
     </div>
     );
